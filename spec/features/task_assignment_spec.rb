07adc0e6ffa0a35b2110b38f00345621b77fae88
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-20 14:55:45 +0400

    Add spec for task assignment.

---
+++ spec/features/task_assignment_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Task Assignment" do
+  let!(:user)   { FactoryGirl.create(:user) }
+  let!(:user_2) { FactoryGirl.create(:user) }
+  let!(:task)   { FactoryGirl.create(:task, user: user) }
+
+  before(:each) do
+    Warden.test_mode!
+    login_as user
+  end
+
+  it "can assign task to other user" do
+    visit "/tasks/#{task.id}/edit"
+
+    select user_2.email, from: "Owner"
+
+    click_button 'Save'
+
+    expect(page).to have_content('Task saved')
+    expect(page).to_not have_content(task.name)
+  end
+end
